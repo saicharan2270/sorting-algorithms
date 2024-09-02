b01f8f22fd98cb6884b3469af1cd81fbe10dbe03
saicharan2270 <156429636+saicharan2270@users.noreply.github.com>
2024-09-03 01:33:44 +0530

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,1 +1,5 @@
+//space complexity - O(logn)
+//but here time complexity is bit different since here worst and average case are different but for the rest of the algorithms it is same .
+//worst case - O(N^2) average case - O(nlogn)
+
 
