16a4bd18ffcb14f835bebe25817b3978b037d7ff
saicharan2270 <156429636+saicharan2270@users.noreply.github.com>
2024-09-02 23:56:29 +0530

    Create mergesort.cpp

---
+++ mergesort.cpp
@@ -0,0 +1,37 @@
+void merge(int arr[],int s,int mid,int e){
+  int leftlen=mid-s+1;
+  int rightlen=e-mid;
+  int leftarr[leftlen];
+  int rightarr[rightlen];
+  for(int i=0;i<leftlen;i++){
+    leftarr[i]=arr[s+i];
+  }
+  for(int i=0;i<rightlen;i++){
+    rightarr[i]=arr[mid+i+1];
+  }
+  int leftindex=0,rightindex=0,mainindex=s;
+  while(leftindex<leftlen&&rightindex<rightlen){
+    if(leftarr[leftindex]<=rightarr[rightindex]){
+      arr[mainindex]=leftarr[leftindex];
+      leftindex++:
+    }
+    else{
+      arr[mainindex]=rightarr[rightindex];
+      rightindex++:
+    }
+    mainindex++;
+  }
+  while(leftindex<leftlen){
+    arr[mainindex++]=leftarr[leftindex++];
+  }
+  while(rightindex<rightlen){
+    arr[mainindex++]=rightarr[rightindex++];
+  }
+}
+void mergesort(int arr[],int s,int e){
+  if(s>=e)return;
+  int mid=s+(e-s)/2;
+  mergesort(arr,s,mid);
+  mergesort(arr,mid+1,e);
+  merge(arr,s,mid,e);
+}
