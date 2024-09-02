bdc8008b50171d5d4d01adda835d67a9dfb9d390
saicharan2270 <156429636+saicharan2270@users.noreply.github.com>
2024-09-02 19:41:53 +0530

    Create bubblesort.cpp

---
+++ bubblesort.cpp
@@ -0,0 +1,14 @@
+void bubblesort(vector<int> arr){
+  int n=arr.size();
+  for(int i=0;i<n-1;i++){
+    bool swapped=false;
+    for(int j=i;j<n-1-i;j++){
+      if(arr[j+1]<arr[i]){
+        swap(arr[j+1],arr[j]);
+        swapped=true;
+    }
+      if(!swapped)break;
+  }
+}
+return;
+}
